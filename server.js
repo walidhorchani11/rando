dc86d02e18c8c9b09e7876a63586c144545fd9c8
Horchani Walid <walidhorchani11@gmail.com>
2020-05-07 22:12:17 +0100

    variable envirenment

--- server.js
+++ server.js
@@ -1,4 +1,9 @@
-const server = require('http').createServer((req, res) => {
+const http = require('http');
+const dotenv = require('dotenv');
+
+dotenv.config({ path: `${__dirname}/config.env` });
+
+const server = http.createServer((req, res) => {
   res.end('hello from server ...');
 });
 
@@ -6,4 +11,5 @@ const PORT = process.env.PORT || 3000;
 
 server.listen(PORT, () => {
   console.log(`listening to server : ${PORT}`);
+  console.log('our NODE_ENV ::: ', process.env.NODE_ENV);
 });
