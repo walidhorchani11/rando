799deb7ef10a665b493cae416da30f577cf0267c
Horchani Walid <walidhorchani11@gmail.com>
2020-05-08 00:30:35 +0100

    serving static file

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const placeRouter = require('./routes/placeRoutes');
 
 const app = express();
 
+app.use(express.static(`${__dirname}/public`));
+
 app.use('/api/v1/places', placeRouter);
 
 module.exports = app;
