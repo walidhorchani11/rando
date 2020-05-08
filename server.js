2efa7e140c8ab2b56325b71de9350a3836f4ef17
walid horchani <walid.horchani@oyez.fr>
2020-05-08 15:22:23 +0100

    connexion to mongo db

--- server.js
+++ server.js
@@ -1,9 +1,24 @@
 const dotenv = require('dotenv');
+const mongoose = require('mongoose');
 
 const app = require('./app');
 
-dotenv.config({ path: `${__dirname}/config.env` });
+dotenv.config({ path: `${__dirname}/config.env`, debug: true });
 
+// **********  cnx to bdd  ***********
+const bdd = process.env.DATABASE.replace('<PASSWORD>', process.env.PASSWORD);
+(async () => {
+  await mongoose.connect(bdd, {
+    useCreateIndex: true,
+    useNewUrlParser: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+  });
+  console.log('connexion etablished with success');
+})();
+// *************************************
+
+console.log('attendant la cnx ...');
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
