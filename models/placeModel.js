b8e057392d728f88721c74e007f7f65ce84b7624
walid horchani <walid.horchani@oyez.fr>
2020-05-08 15:59:26 +0100

    model & schema Place

---
+++ models/placeModel.js
@@ -0,0 +1,58 @@
+const mongoose = require('mongoose');
+
+const placeSchema = mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+    maxLength: [40, 'A place title must have at least 40 characters'],
+  },
+
+  nbrViews: {
+    type: Number,
+    default: 0,
+    required: false,
+  },
+
+  description: {
+    type: String,
+    required: false,
+  },
+
+  createdAt: {
+    type: Date,
+    default: new Date(),
+  },
+
+  nbrLikes: {
+    type: Number,
+    default: 0,
+  },
+
+  share: {
+    type: Boolean,
+    default: true,
+  },
+
+  images: {
+    type: [String],
+    required: false,
+  },
+
+  cost: {
+    type: Number,
+    required: false,
+  },
+
+  location: {
+    type: String,
+    required: false,
+  },
+
+  adress: {
+    type: String,
+    required: false,
+  },
+  // user proprietaire
+});
+
+module.exports = mongoose.model('Place', placeSchema);
