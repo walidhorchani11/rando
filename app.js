b8076fd0c1220fc985571eedffa536938bc824b6
Horchani Walid <walidhorchani11@gmail.com>
2020-05-08 01:24:37 +0100

    use morgan pckage for dev env

--- app.js
+++ app.js
@@ -1,8 +1,16 @@
 const express = require('express');
+const morgan = require('morgan');
+const dotenv = require('dotenv');
 const placeRouter = require('./routes/placeRoutes');
 
+dotenv.config({ path: `${__dirname}/config.env`});
 const app = express();
 
+console.log('in app express env is ::: ', process.env.NODE_ENV);
+
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
 app.use(express.static(`${__dirname}/public`));
 
 app.use('/api/v1/places', placeRouter);
