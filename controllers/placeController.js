121af610f55774e0dae13abc0961aef227376a59
Horchani Walid <walidhorchani11@gmail.com>
2020-05-10 00:18:39 +0100

    refact

--- controllers/placeController.js
+++ controllers/placeController.js
@@ -1,50 +1,12 @@
 const Place = require('../models/placeModel');
+const ApiFeatures = require('../utils/apiFeatures');
 
 exports.getAllPlaces = async (req, res, next) => {
   try {
-    let query = Place.find();
+    const apiFeatures = new ApiFeatures(req.query, Place.find());
+    const queryObj = apiFeatures.filter().select().sort().page();
 
-    // ********** filter ********
-    // avec postman : ?cost[gte]=50,nbrLikes=10,sort=-cost,page.....
-    // on la recus : cost: {gte:50} // noter labsence du $
-    // on aura aussi les sort, page limit fields qu on doit les exclure
-    console.log('queryyyyy:::', req.query);
-    let queryObj = {...req.query};
-    const excluded = ['sort', 'fields', 'page', 'limit'];
-    excluded.map(elem => delete queryObj[elem]);
-
-    // ajout du signe $
-    let queryStr = JSON.stringify(queryObj);
-    queryStr = queryStr.replace(/\b(lte|lt|gte|gt)\b/g, match => `$${match}`);
-    query = query.find(JSON.parse(queryStr));
-
-    // *********paginatin & limit ********
-    const page = req.query.page * 1 || 1;
-    const limit = req.query.limit * 1 || 10;
-    // page 2 => 11 - 20; page 3 => 21 - 30
-    const skip = (page - 1) * limit;
-    query.skip(skip).limit(limit);
-
-    // ********** selection  ***********
-    // {fields: "title,cost,description"}
-    if (req.query.fields) {
-      const selectedFields = req.query.fields.split(',').join(' ');
-      query.select(selectedFields);
-    } else {
-      query.select('-__v');
-    }
-
-    // ******* sort *********
-    if (req.query.sort) {
-      // i can receive => {sort: "-price,duration"}
-      // formatage:
-      const sortBy = req.query.sort.split(',').join(' ');
-      query.sort(sortBy);
-    } else {
-      query.sort('-createdAt');
-    }
-    // **********************
-    const places = await query;
+    const places = await queryObj.queryMongoose;
     res.status(200).json({
       status: "success",
       results: places.length,
