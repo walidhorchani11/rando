e5f33bd5d66e638b058ed8ea27996ab176e684e9
walid horchani <walid.horchani@oyez.fr>
2020-05-12 15:50:53 +0100

    Error middleware

--- app.js
+++ app.js
@@ -15,4 +15,29 @@ app.use(express.static(`${__dirname}/public`));
 
 app.use('/api/v1/places', placeRouter);
 
+// pour les routes inexistant
+app.all('*', (req, res, next) => {
+  // res.status(404).json({
+  //   status: 'fail',
+  //   message: `path ${req.originalUrl} not found!`,
+  // });
+
+  // creation d error
+  const error = new Error(`paath ${req.originalUrl} not found!`);
+  error.status = 'fail';
+  error.statusCode = 404;
+
+  next(error);
+});
+
+app.use((error, req, res, next) => {
+  error.status = error.status || 'fail';
+  error.statusCode = error.statusCode || 500;
+
+  res.status(error.statusCode).json({
+    status: error.status,
+    message: error.message,
+  });
+});
+
 module.exports = app;
