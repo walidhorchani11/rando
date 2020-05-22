26f2a4f490f9a3b9b9670d9eeb8ed6b091144cf3
walid horchani <walid.horchani@oyez.fr>
2020-05-22 16:02:16 +0100

    prod & dev error

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,10 +1,38 @@
+const sendErrorProd = (error, res) => {
+  // send minim of information
+  // test if we have trusted error isOperational is true or other
+  if (error.isOperational) {
+    res.status(error.statusCode).json({
+      status: error.status,
+      message: error.message,
+    });
+  } else {
+    //not trusted error
+    res.status(500).json({
+      status: 'error',
+      message: 'somthing went very wrong',
+    });
+  }
+};
+
+const sendErrorDev = (error, res) => {
+  // en mode development, send max info
+  res.status(error.statusCode).json({
+    status: error.status,
+    message: error.message,
+    stack: error.stack,
+    error,
+  });
+};
+
 module.exports = (error, req, res, next) => {
   console.log(error.stack);
   error.status = error.status || 'fail';
   error.statusCode = error.statusCode || 500;
 
-  res.status(error.statusCode).json({
-    status: error.status,
-    message: error.message,
-  });
+  if (process.env.NODE_ENV !== 'production') {
+    sendErrorProd(error, res);
+  } else {
+    sendErrorDev(error, res);
+  }
 };
